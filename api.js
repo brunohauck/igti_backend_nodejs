c532c11b289baaafdee0c877a657aa0013c2da83
Bruno Hauck <brunohauck@gmail.com>
2019-06-15 23:51:00 -0300

    Segundo commit com código fonte

---
+++ api.js
@@ -0,0 +1,157 @@
+const _ = require('lodash');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+var db = require('./modules/daomodule');
+
+
+var USERS = [
+    { 
+        'name': 'Bruno de Líbero Hauck Ferreira', 
+        'username': 'brunohauck',
+        'email':'brunohauck@gmail.com', 
+        'phone':'(31)988724779', 
+        'password': '123456', 
+        'img_url':'https://media.licdn.com/dms/image/C4D03AQGkbLIovbjpEQ/profile-displayphoto-shrink_200_200/0?e=1559174400&v=beta&t=AX0Zd0cT36yL8g2OAFvKCo9K408Mms4YU_Ky4KLIz5g' },
+    { 
+        'name': 'André Pereira', 
+        'username': 'pereira',
+        'email':'apereirahauck@gmail.com', 
+        'phone':'(31)111111111', 
+        'password': '123456', 
+        'img_url':'https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png' },
+];
+
+var PRODUCTS = [
+    { 
+        'productName': 'Curso IONIC 4', 
+        'price': 39.99, 
+        'description': 'Curso IONIC 4 do básico ao avançado', 
+        'img_url':'https://i.udemycdn.com/course/240x135/1325912_2431.jpg' },
+    { 
+        'productName': 'Curso Angular 7 com Node Js e Mongo DB', 
+        'price': 39.99, 
+        'description': 'Curso Angular 7 com Mongo DB do básico ao avançado', 
+        'img_url':'https://udemy-images.udemy.com/course/480x270/833442_b26e_3.jpg' 
+    },
+];
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTION");
+    next();
+});
+
+app.use(bodyParser.json());
+//app.use(expressJwt({secret: 'fdsfljalfldajfldafjdlkafljdaa'}).unless({path: ['/api/auth']}));
+
+app.get('/', function (req, res) {
+    res.send('API Server')
+});
+
+app.post('/api/auth', function(req, res) {
+    const body = req.body;
+    const user = USERS.find(user => user.username == body.username);
+    if(!user || body.password != '123456') return res.sendStatus(401);
+    var token = jwt.sign({userID: user.id}, 'fdsfljalfldajfldafjdlkafljdaa', {expiresIn: '2h'});
+    res.send({token});
+});
+
+app.post('/api/createuser', function(req, res) {
+    const user = req.body;    
+    db.createUser(user, function(err, value) {
+        if (err) throw err;
+        console.log(value);
+        if(value){
+            res.send({"msg": "sucesso"});
+        }else{
+            res.send({"msg": "erro"});
+        }
+    });        
+});
+
+app.post('/api/edituser', function(req, res) {
+    const user = req.body;   
+    console.log(user);     
+    db.editUser( user, function(err, value) {
+        if (err) throw err;
+        console.log(value);
+        if(value){
+            res.send({"msg": "sucesso"});
+        }else{
+            res.send({"msg": "erro"});
+        }
+    });        
+});
+
+app.delete('/api/deleteuser/:id', function(req, res) {
+    console.log("----->delete")
+    const id = req.params.id;   
+    console.log(id);     
+    db.deleteUser( id, function(err, value) {
+        if (err) throw err;
+        console.log(value);
+        if(value){
+            res.send({"msg": "sucesso"});
+        }else{
+            res.send({"msg": "erro"});
+        }
+    });        
+});
+
+app.get('/api/users', function (req, res){
+    res.type('json');
+    db.dbGetAllUsers( function(err, value) {
+        if (err) throw err;
+        console.log(value);
+        res.send(value);
+    });
+});
+
+app.get('/api/products', function (req, res){
+    res.type('json');
+    db.dbGetAllProducts( function(err, value) {
+        if (err) throw err;
+        console.log(value);
+        res.send(value);
+    });
+});
+
+app.get('/api/createcollections', function (req, res){
+    res.type('json');
+    //db.createUsersColetion();
+    console.log("Criando UserCollection");
+    //db.createUsers(USERS);
+    db.createProductsCollection();
+    db.createProducts(PRODUCTS);
+    console.log("Salvando os usuários estáticos no banco de dados");
+});
+
+app.get('/api/getAllUsers', function (req, res){
+    res.type('json');
+    db.dbGetAllUsers( function(err, value) {
+        if (err) throw err;
+        console.log(value);
+        res.send(value);
+    });
+});
+
+app.get('/api/getUserById/:id', function (req, res){
+    var user_id = req.params.id;
+    res.type('json');
+    console.log(user_id);
+    db.dbGetUserById( user_id, function(err, value) {
+        if (err) throw err;
+        console.log(value);
+        res.send(value);
+    });
+});
+
+app.listen(4000, function(){
+    console.log('Listening on por 4000');
+});
+
---
+++ modules/daomodule.js
@@ -0,0 +1,198 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017";
+exports.createUsersColetion = function (){
+    MongoClient.connect(url , { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.createCollection("users", function(err, res) {
+          if (err) throw err;
+          console.log("Users Collection created!");
+          db.close();
+        });
+      }); 
+};
+
+exports.createUsers = function (users){
+    MongoClient.connect(url, { useNewUrlParser: true } , function(err, db ) {
+        if(err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("users").insertMany(users, function(err, res){
+            if(err) throw err;
+            db.close();
+        });
+    });
+}
+
+exports.createUser = function (user,callback){
+    MongoClient.connect(url , { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("users").insertOne(user, function(err, res) {
+            if(err)
+            {
+              callback(err,false);
+            }
+            else{
+                var result = true
+                db.close();
+                callback(null,result);
+            }            
+        });
+    });
+};
+
+
+exports.deleteUser = function (id,callback){
+    var ObjectID = require('mongodb').ObjectID; 
+    MongoClient.connect(url , { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var query = { _id : ObjectID(id) };
+        dbo.collection("users").deleteOne(query,  { upsert: true }, function(err, res) {
+            if(err)
+            {
+              callback(err,false);
+            }
+            else{
+                var result = true
+                db.close();
+                callback(null,result);
+            }            
+        });
+    });
+};
+
+exports.editUser = function (user,callback){
+    var ObjectID = require('mongodb').ObjectID; 
+    MongoClient.connect(url , { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        var query = { _id : ObjectID(user._id) };
+        var newvalues = 
+        { $set: 
+            { 
+            "name": user.name, 
+            "username": user.username,
+            "email": user.email,
+            "phone": user.phone,
+            "password": user.password,
+            },
+        };
+        dbo.collection("users").updateOne(query, newvalues, { upsert: true }, function(err, res) {
+            if(err)
+            {
+              callback(err,false);
+            }
+            else{
+                var result = true
+                db.close();
+                callback(null,result);
+            }            
+        });
+    });
+};
+
+exports.dbGetUserById = function (userId,callback) {
+    var ObjectID = require('mongodb').ObjectID; 
+    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("mydb");
+      var query = { _id : ObjectID(userId) };
+      dbo.collection('users').find(query).toArray(function(err,result){
+        if(err)
+        {
+          callback(err,null);
+        }
+        if(result)
+          callback(null,result);
+      });
+    });
+};
+
+exports.dbGetAllUsers = function (callback){
+    MongoClient.connect(url , { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("users").find({}).toArray(function(err, result) {
+            if(err)
+            {
+              callback(err,null);
+            }
+            if(result){
+                db.close();
+                callback(null,result);
+            }            
+        });
+    });
+};
+
+//------------------------------------Products-------------------------------------------------------------
+
+//Cria uma função já no padrão ES6 
+exports.createProductsCollection = function (){
+    // Conecta no banco de dados
+    MongoClient.connect(url , { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        // Define o nome do banco de dados
+        var dbo = db.db("mydb");
+        // Cria a col
+        dbo.createCollection("products", function(err, res) {
+          // Caso der erro imprime o erro no terminal  
+          if (err) throw err;
+          console.log("Products Collection created!");
+          // Fexa a conexão com banco de dados
+          db.close();
+        });
+      }); 
+};
+
+
+// foi criada um função onde passamos por parâmetro nosso Json 
+exports.createProducts = function (products){
+    // conecta no banco de dados
+    MongoClient.connect(url, { useNewUrlParser: true } , function(err, db ) {
+        if(err) throw err;
+        // define qual é banco de dados
+        var dbo = db.db("mydb");
+        // insere um Json com uma lista de produtos
+        // Obs a função "insertMany" é especícica para uma lista
+        // Para inserir somente um produto você deve usar "insertOne"
+        dbo.collection("products").insertMany(products, function(err, res){
+            if(err) throw err;
+            db.close();
+        });
+    });
+}
+
+// um ponto importante dessa função e definição de um callback que 
+// vai retornar a informação para o ponto do sistema que executamos
+// essa função assincrona que no caso é nosso arquivo api.js
+exports.dbGetAllProducts = function (callback){
+    MongoClient.connect(url , { useNewUrlParser: true } , function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        // realiza um find na coleção products que vai retornar um Json com todos
+        // os produtos cadastrados nessa coleção. 
+        dbo.collection("products").find({}).toArray(function(err, result) {
+            if(err)
+            {
+              // em caso de erro retorna o erro  
+              callback(err,null);
+            }
+            if(result){
+                db.close();
+                // em caso de sucesso será retornado a lista de produtos cadastrada no
+                // banco de dados
+                callback(null,result);
+            }            
+        });
+    });
+};
+
+
+
+
+
+
+
+
